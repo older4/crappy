5a14ac25fce052553aa61d0b7ea20302e7ca0346
ryo <ryo@fake.com>
2020-01-09 21:41:35 +0900

    おそらくV4-V5.2に対応

--- scripts/pcdata.py
+++ scripts/pcdata.py
@@ -11,4 +11,4 @@ if len(sys.argv) < 2:
 
 data = get_ransom_data(sys.argv[1], "PC DATA")
 decr = ARC4.new(b".oj=294~!z3)9n-1,8^)o((q22)lb$").decrypt(data)
-print(codecs.decode(decr, "utf-16le"))
+print(codecs.decode(decr, "utf-8"))
--- scripts/tools.py
+++ scripts/tools.py
@@ -1,5 +1,6 @@
 from itertools import chain, takewhile
 import base64
+import re
 
 
 def int_from_bytes(bytes_, byteorder):
@@ -19,3 +20,16 @@ def get_ransom_data(path, name="GANDCRAB KEY"):
         out = "".join(chain.from_iterable(takewhile(lambda x: x != end, f)
                                           for line in f if line == st))
     return base64.b64decode(out)
+
+def get_ransom_ver(path, name="GANDCRAB KEY"):
+    with open(path, "r", encoding="utf-16") as f:
+        ver_text = f.readline()
+
+    ver = re.search("(\d|\.){1,}",ver_text).group()
+    # Like 5.0.4 has two dot, so it is not float.
+    # Convert 5.0.4 to 5.04
+    tmp = re.findall(r"\.\d",ver)
+    if(len(tmp)>1):
+        ver = re.sub(r"\.\d$",tmp[-1].strip("."),ver)
+
+    return float(ver) 
